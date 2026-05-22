9d0a8a2edd52ac76ff3406c82c5ef62c678a6506
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-22 17:07:41 +0200

    Compleated Step 2 (Add more Responces): with the classic 20 8-ball answers

--- magic_8_ball.py
+++ magic_8_ball.py
@@ -8,7 +8,32 @@ This program simulates the Magic 8 Ball by generating random responses to user q
 import random # Import the random module to generate random numbers
 
 # List of possible responses for the Magic 8 Ball
-responses = ["YES", "NO", "MAYBE"]
+# responses = ["YES", "NO", "MAYBE"]
+responses = [
+    # Affirmative Answers (Yes)
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    # Neutral Answers (Maybe / Try Again)
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    # Negative Answers (No)
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+]
 
 # Welcome message
 print("--- Welcome to the Magic 8 Ball! ---")
