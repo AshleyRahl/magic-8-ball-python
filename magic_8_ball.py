57ad0be688fe4125d5267836cd2471dad5a67805
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-22 16:38:38 +0200

    create list with 3 potential ansers and import random module for later use

---
+++ magic_8_ball.py
@@ -0,0 +1,11 @@
+"""
+Magic 8 Ball:
+A Magic 8 Ball is a toy used for fortune-telling or seeking advice. 
+It provides random answers to yes-or-no questions. 
+This program simulates the Magic 8 Ball by generating random responses to user questions.
+"""
+
+import random # Import the random module to generate random numbers
+
+# List of possible responses for the Magic 8 Ball
+responses = ["YES", "NO", "MAYBE"]
